9100e74a0692fbcb4577467f57f537c9426aa662
Jorge <newdarkjg@gmail.com>
2024-04-21 16:38:34 +0200

    Login y Actualizacion comprobado

--- Back/src/controller/CuentaController.js
+++ Back/src/controller/CuentaController.js
@@ -6,26 +6,35 @@ const { generateToken, verifyToken } = require('../middlewares/authMiddleware');
 
 const CuentaController = {
     async registro(req, res) {
-        const { email, password, Productos } = req.body;        
-       
+        const { email, password, neveraId } = req.body;
+
         const Cuentas = await Cuenta.find();
-        const user = Cuentas.find((user) => user.email === email );
-        console.log(user)
-        if(!user){
-            const creada= await Cuenta.create(req.body);
-            res.send(JSON.stringify(creada))
-        }else{
-            res.send(JSON.stringify("Error la cuenta ya existe"))
-        }
-       
-    },
-    async ActualizarUsuarioNevera(req, res) {
-        const user = req.userCredential.user
+        const user = Cuentas.find((user) => user.email === email);
         console.log(user)
         if (!user) {
-            await user?.updateNeveraID(req.IdNevera)
+            const creada = await Cuenta.create(req.body);
+            res.send(JSON.stringify(creada))
+        } else {
+            res.send(JSON.stringify("Error la cuenta ya existe"))
         }
-        console.log(user)
+
+    },
+    async ActualizarUsuarioNevera(req, res) {
+        const { email, password, neveraId } = req.body;
+        try {
+            const user = await Cuenta.findOne({ email: email });
+    
+            if (user) {
+                user.neveraId = neveraId;
+                const actualizada = await user.save();
+                res.send(JSON.stringify(actualizada));
+            } else {
+                res.send(JSON.stringify("Error, cuenta no encontrada"));
+            }
+        } catch (error) {
+            console.error(error);
+            res.status(500).send(JSON.stringify("Error interno del servidor"));
+        }
     },
 
     async Login(req, res) {
@@ -33,17 +42,17 @@ const CuentaController = {
 
         const Cuentas = await Cuenta.find();
 
-        const user = Cuentas.find((user) => user.email === email  && user.password === password);
+        const user = Cuentas.find((user) => user.email === email && user.password === password);
 
         console.log(user)
         if (user) {
             const token = generateToken(user);
             req.session.token = token;
             console.log(req.session.cookie)
-            res.send({"user":user,"token":token})
-        }else {
-        res.status(401).json({ mensaje: 'Credenciales incorrectas' });
-    }
+            res.send({ "user": user, "token": token })
+        } else {
+            res.status(401).json({ mensaje: 'Credenciales incorrectas' });
+        }
     }
 
 }
